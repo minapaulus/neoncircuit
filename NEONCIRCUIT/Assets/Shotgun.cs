b499301efab164f40509a36b37ecf699f7a63a7c
minapaulus <minapaulus@gmail.com>
2021-08-10 13:20:04 +0200

    prefabs, sounds, shotgun (funktioniert noch nicht ganz aber das script an sich ist fertig)

--- NEONCIRCUIT/Assets/Shotgun.cs
+++ NEONCIRCUIT/Assets/Shotgun.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Shotgun : MonoBehaviour
 {
     public float damage = 20f;
-    public float range = 300f;
+    public float range = 100f;
     public Camera cam;
     public GameObject impactEffect;
     public float impactForce = 200f;
@@ -11,13 +11,15 @@ public class Shotgun : MonoBehaviour
     public double firerate = 2;
     private GameObject _player;
     public Enemy.AssignedColors color;
-    private Quaternion v;
     public Playerstats playerstats;
+    public AudioClip[] sgsounds;
+    private AudioSource sasource;
 
     void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
         playerstats.ChangePrimaryColor(color);
+        sasource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -37,26 +39,47 @@ public class Shotgun : MonoBehaviour
     void Shoot()
     {
 
-        Debug.Log("SHOOOT");
-
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, cam.transform.forward, out hit, range))
+        if (playerstats.CanFireSecondary())
         {
-            if (playerstats.CanFireSecondary())
+            for(int i = 0; i <= 25; i++)
             {
-                if (hit.transform.tag == "Hitbox")
+                var tilt = Quaternion.AngleAxis(Random.Range(0f, 45f), Vector3.up);
+                var spin = Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.forward);
+                var tiltspin = spin * tilt;
+                //var test = cam.transform.up * Random.Range(0.0f, 0.2f);
+                //var vector = cam.transform.forward * 10f + Quaternion.AngleAxis(Random.Range(0.0f, 360.0f), cam.transform.forward) * test;
+                Ray ray = new Ray(transform.position, tiltspin * cam.transform.forward);
+                RaycastHit hit;
+                Debug.Log("test");
+                if (Physics.Raycast(ray, out hit, range))
                 {
-                    var hbox = hit.transform.GetComponent<Hitbox>();
-                    hbox.Damage(_player, color, damage);
+                    Debug.Log(hit.transform.tag);
+                    if (hit.transform.tag == "Hitbox")
+                    {
+                        var hbox = hit.transform.GetComponent<Hitbox>();
+                        hbox.Damage(_player, color, damage);
+                    }
+                    if (hit.rigidbody != null)
+                    {
+                        hit.rigidbody.AddForce(-hit.normal * impactForce);
+                    }
+                    playSound();
+                    GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(impact, 0.3f);
                 }
-                if (hit.rigidbody != null)
-                {
-                    hit.rigidbody.AddForce(-hit.normal * impactForce);
-                }
-                GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-                Destroy(impact, 0.3f);
             }
+            playerstats.AddSecondary(-1);
+            //if (Physics.Raycast(transform.position, cam.transform.forward, out hit, range))
         }
+    }
+
+    void playSound()
+    {
+        var i = Random.Range(1, sgsounds.Length);
+        sasource.clip = sgsounds[i];
+        sasource.PlayOneShot(sasource.clip);
+        sgsounds[i] = sgsounds[0];
+        sgsounds[0] = sasource.clip;
     }
 }
 
