a07d40208a04f132a7182b5cb151a75c2511cda0
Niklas Braun <NiklasBraun93@googlemail.com>
2021-08-14 16:16:50 +0200

    Roof added, grenade modified

--- NEONCIRCUIT/Assets/grenade.cs
+++ NEONCIRCUIT/Assets/grenade.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class grenade : MonoBehaviour
 {
     public GameObject Throwable;
-    public float throwStrength; 
+    public float throwStrength;
+    public Playerstats playerstat;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(1)){
-            var grenadina = Instantiate(Throwable, transform.position, transform.rotation);
-            grenadina.GetComponent<Rigidbody>().AddForce(transform.forward * throwStrength, ForceMode.VelocityChange);
+            if (playerstat.CanFireSecondary())
+            {
+                playerstat.AddSecondary(-1f);
+                var grenadina = Instantiate(Throwable, transform.position, transform.rotation);
+                grenadina.GetComponent<Rigidbody>().AddForce(transform.forward * throwStrength, ForceMode.VelocityChange);
+            }
         }
     }
 }
